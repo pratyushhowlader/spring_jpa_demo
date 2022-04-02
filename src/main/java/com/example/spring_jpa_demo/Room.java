9efa94111407b894d8ed728260fe5ab597818dd5
Prasun <prasunhowlader@yahoo.com>
2022-04-02 20:31:19 +0600

    New comment added

--- src/main/java/com/example/spring_jpa_demo/Room.java
+++ src/main/java/com/example/spring_jpa_demo/Room.java
@@ -1,7 +1,7 @@
 package com.example.spring_jpa_demo;
 
 import javax.persistence.*;
-//Pratyush asdfadsf
+//Pratyush asdfadsfasfasfasf asfdasf
 @Entity
 @Table(name = "tbl_room")
 public class Room {
